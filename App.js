cb8df2812278a4d1836dcc7071e363ed7a63f5b9
lunzhang <lun.zhang1993@gmail.com>
2019-06-25 14:53:52 -0400

    Hide StatusBar and navigation header

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { StatusBar } from 'react-native';
 import Home from './src/containers/Home';
 import Game from './src/containers/Game';
+
+StatusBar.setHidden(true);
 
 const MainNavigator = createStackNavigator({
   Home: { screen: Home },
   Game: { screen: Game },
+},{
+  headerMode: 'none'
 });
 
 const App = createAppContainer(MainNavigator);
