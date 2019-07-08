c911fe0872dd59d73155f956f6afbc37dfa9888a
lunzhang <lun.zhang1993@gmail.com>
2019-07-08 17:28:27 -0400

    Refactor Games

--- src/containers/Games/index.js
+++ src/containers/Games/index.js
@@ -1,39 +1,24 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import { COUNT_DOWN_GAME, WHEN_WHITE_GAME, LOADING_GAME } from '../../constants';
 import CountDown from './CountDown';
 import WhenWhite from './WhenWhite';
 import Loading from './Loading';
 
 function Games({ gameType, gameRef }) {
-    const renderGame = () => {
-        switch (gameType) {
-            case COUNT_DOWN_GAME: {
-                return <CountDown ref={gameRef} />;
-            }
-            case WHEN_WHITE_GAME: {
-                return <WhenWhite ref={gameRef} />;
-            }
-            case LOADING_GAME: {
-                return <Loading ref={gameRef} />;
-            }
-            default: {
-                return <Loading ref={gameRef} />;
-            }
+    switch (gameType) {
+        case COUNT_DOWN_GAME: {
+            return <CountDown ref={gameRef} />;
         }
-    };
-    return (
-        <View style={styles.container}>
-            {renderGame()}
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexGrow: 1,
+        case WHEN_WHITE_GAME: {
+            return <WhenWhite ref={gameRef} />;
+        }
+        case LOADING_GAME: {
+            return <Loading ref={gameRef} />;
+        }
+        default: {
+            return <Loading ref={gameRef} />;
+        }
     }
-});
+};
 
 export default Games;
